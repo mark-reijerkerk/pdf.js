f0aa87d4451f6b82715f8ae1cb0d9802437776b7
Nicky Chen <chenjinlong2016@outlook.com>
2020-08-15 22:46:45 +0800

    Include Japanese translation.

--- gulpfile.js
+++ gulpfile.js
@@ -68,7 +68,7 @@ var COMMON_WEB_FILES = ["web/images/*.{png,svg,gif,cur}", "web/debugger.js"];
 var MOZCENTRAL_DIFF_FILE = "mozcentral.diff";
 
 // Locales used in Edge Translate
-var EDGE_TRANSLATE_LOCALES = ["en-CA", "en-GB", "en-US", "ru", "zh-CN", "zh-TW"];
+var EDGE_TRANSLATE_LOCALES = ["en-CA", "en-GB", "en-US", "ru", "zh-CN", "zh-TW", "ja"];
 
 var REPO = "git@github.com:mozilla/pdf.js.git";
 var DIST_REPO_URL = "https://github.com/mozilla/pdfjs-dist";
