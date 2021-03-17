a9d3ed9072582d09e5b857f7bd5c389a8d8b8b8e
Nicky Chen <chenjinlong2016@outlook.com>
2021-03-17 15:25:40 +0800

    wip: different icon for dark mode and light mode

--- web/pdf_dark_mode.js
+++ web/pdf_dark_mode.js
@@ -1,50 +1,92 @@
 /**
- * @property {HTMLButtonElement} button
+ * @type {String} localStorage key of dark mode setting.
+ */
+const STORAGE_KEY = "pdfjs.dark_mode";
+
+/**
+ * @type {String} dark mode style element id.
+ */
+const STYLE_ELEMENT_ID = "pdfjs-dark-mode-style";
+
+/**
+ * @type {String} dark mode style sheet.
+ */
+const STYLE_ELEMENT_CONTENT_DARK = `
+.textLayer {
+  background: black;
+}
+
+.toolbarButton.darkMode::before,
+.secondaryToolbarButton.darkMode::before {
+  content: var(--toolbarButton-darkMode-dark-icon);
+  transform: rotate(-0.1turn);
+}
+`;
+
+/**
+ * @type {String} light mode style sheet.
+ */
+const STYLE_ELEMENT_CONTENT_LIGHT = `
+.textLayer {
+  background: white;
+}
+
+.toolbarButton.darkMode::before,
+.secondaryToolbarButton.darkMode::before {
+  content: var(--toolbarButton-darkMode-light-icon);
+}
+`;
+
+/**
+ * PDF dark mode manager.
  *
- * @property {EventBus} eventBus
+ * @property {HTMLButtonElement} button toggle button
+ *
+ * @property {EventBus} eventBus event bus
  */
 class PDFDarkMode {
   /**
-   *
-   * @param {HTMLButtonElement} button
-   * @param {EventBus} eventBus
+   * @param {HTMLButtonElement} button toggle button
+   * @param {EventBus} eventBus event bus
    */
   constructor(button, eventBus) {
     this.button = button;
     this.eventBus = eventBus;
 
+    // Create dark mode style element.
+    this.styleElement = document.createElement("style");
+    document.head.appendChild(this.styleElement);
+    this.styleElement.id = STYLE_ELEMENT_ID;
+
     // Check dark mode option.
-    const inDarkMode = localStorage.getItem("pdfjs.dark_mode") || "false";
+    const inDarkMode = localStorage.getItem(STORAGE_KEY) || "false";
     if (inDarkMode === "true") {
-      const cssElement = document.createElement("style");
-      cssElement.id = "pdfjs-darkmode-style";
-      cssElement.textContent = ".textLayer { background: black; }";
-      document.head.appendChild(cssElement);
+      this.enable();
+    } else {
+      this.disable();
     }
 
     this.eventBus.on("darkmode", this.toggle.bind(this));
   }
 
   toggle() {
-    const inDarkMode = localStorage.getItem("pdfjs.dark_mode") || "false";
+    const inDarkMode = localStorage.getItem(STORAGE_KEY) || "false";
     if (inDarkMode === "false") {
-      const cssElement = document.createElement("style");
-      cssElement.id = "pdfjs-darkmode-style";
-      cssElement.textContent = ".textLayer { background: black; }";
-      document.head.appendChild(cssElement);
-      localStorage.setItem("pdfjs.dark_mode", "true");
+      this.enable();
+      localStorage.setItem(STORAGE_KEY, "true");
     } else {
-      const cssElement = document.getElementById("pdfjs-darkmode-style");
-      if (cssElement) {
-        cssElement.remove();
-      }
-      localStorage.setItem("pdfjs.dark_mode", "false");
+      this.disable();
+      localStorage.setItem(STORAGE_KEY, "false");
     }
   }
 
-  enable() {}
+  enable() {
+    this.styleElement.textContent = STYLE_ELEMENT_CONTENT_DARK;
+  }
 
-  disable() {}
+  disable() {
+    this.styleElement.textContent = STYLE_ELEMENT_CONTENT_LIGHT;
+  }
 }
 
 export { PDFDarkMode };
