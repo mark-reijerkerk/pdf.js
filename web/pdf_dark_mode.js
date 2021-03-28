7687dab6f25a85151e52807f1931622fb1ed8aa3
Nicky Chen <chenjinlong2016@outlook.com>
2021-03-28 19:26:30 +0800

    feat: add auto (follow the system) option to dark mode options

--- web/pdf_dark_mode.js
+++ web/pdf_dark_mode.js
@@ -36,6 +36,24 @@ const STYLE_ELEMENT_CONTENT_LIGHT = `
 `;
 
 /**
+ * @type {String} auto mode style sheet.
+ */
+const STYLE_ELEMENT_CONTENT_AUTO = `
+@media (prefers-color-scheme: dark) {
+  .thumbnailImage, .pdfViewer .page {
+    filter: brightness(0.85) grayscale(0.15) invert(1.0) hue-rotate(0.5turn);
+    border-image: none;
+    box-shadow: none;
+  }
+}
+
+.toolbarButton.darkMode::before,
+.secondaryToolbarButton.darkMode::before {
+  content: var(--toolbarButton-darkMode-auto-icon);
+}
+`;
+
+/**
  * PDF dark mode manager.
  *
  * @property {HTMLButtonElement} button toggle button
@@ -57,25 +75,44 @@ class PDFDarkMode {
     this.styleElement.id = STYLE_ELEMENT_ID;
 
     // Check dark mode option.
-    const inDarkMode = localStorage.getItem(STORAGE_KEY) || "false";
-    if (inDarkMode === "true") {
-      this.enable();
-    } else {
-      this.disable();
+    const darkModeOption = localStorage.getItem(STORAGE_KEY) || "auto";
+    switch (darkModeOption) {
+      case "dark":
+        this.enable();
+        break;
+      case "light":
+        this.disable();
+        break;
+      case "auto":
+      default:
+        this.auto();
+        break;
     }
 
     this.eventBus.on("darkmode", this.toggle.bind(this));
   }
 
   toggle() {
-    const inDarkMode = localStorage.getItem(STORAGE_KEY) || "false";
-    if (inDarkMode === "false") {
-      this.enable();
-      localStorage.setItem(STORAGE_KEY, "true");
-    } else {
-      this.disable();
-      localStorage.setItem(STORAGE_KEY, "false");
+    const darkModeOption = localStorage.getItem(STORAGE_KEY) || "auto";
+    switch (darkModeOption) {
+      case "auto":
+        this.disable();
+        localStorage.setItem(STORAGE_KEY, "light");
+        break;
+      case "light":
+        this.enable();
+        localStorage.setItem(STORAGE_KEY, "dark");
+        break;
+      case "dark":
+      default:
+        this.auto();
+        localStorage.setItem(STORAGE_KEY, "auto");
+        break;
     }
+  }
+
+  auto() {
+    this.styleElement.textContent = STYLE_ELEMENT_CONTENT_AUTO;
   }
 
   enable() {
